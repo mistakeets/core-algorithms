90ef252dee8fae4b7f246844e1228248ca2a3c37
Diana Vashti <diana.vashti@gmail.com>
2017-01-24 09:54:36 -0800

    wups

--- src/fibonacci.js
+++ src/fibonacci.js
@@ -1,3 +1,13 @@
 export default function fibonacci(n) {
+  if (n === 1){
+    return [0]
+  } else if (n >= 2)
+  let fibArray = [0,1]
+  for (var i=1; i <= n, i++){
+
+  }
+
+
+    return fibArray
 
 }
